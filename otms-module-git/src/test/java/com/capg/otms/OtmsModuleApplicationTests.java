a6285a0b0f592bd578eb9f4a7b29da55896ce6f3
dell <dell@DESKTOP-BGCHS08>
2020-09-23 09:39:20 +0530

    commit

---
+++ otms-module-git/src/test/java/com/capg/otms/OtmsModuleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.capg.otms;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.capg.otms.question.OtmsModuleApplication;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=OtmsModuleApplication.class)
+class OtmsModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
